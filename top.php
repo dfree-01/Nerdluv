717f1aef0eefed1a108a373d138315e936455c30
Demari <113479556+dfree-01@users.noreply.github.com>
2024-03-19 11:52:35 -0400

    Add files via upload

---
+++ top.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>NerdLuv</title>
+    <link href="heart.gif" type="image/gif" rel="shortcut icon">
+    <link href="nerdieluv.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+<div id="bannerarea">
+    <img src="nerdluv.png" alt="banner logo"> <br>
+    where meek geeks meet
+</div>
